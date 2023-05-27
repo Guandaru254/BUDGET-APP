21bd41af82a94a839b9ab9e759b792ad94cf28b6
Guandaru254 <leroykim82@gmail.com>
2023-05-27 21:01:33 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ /*const App = () => {
+    return
+    <h1>Welcome To React</h1>;
+}; */
+
+ReactDOM.render (
+    <App />,
+    document.getElementById ('root')
+);
