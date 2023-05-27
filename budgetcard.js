efe5085af75cbd956f1b728676886ed7ce29f6ef
Guandaru254 <leroykim82@gmail.com>
2023-05-27 21:02:32 +0300

    Add files via upload

---
+++ budgetcard.js
@@ -0,0 +1,19 @@
+import {Card} from "react-bootstrap" 
+
+import { currencyFormatter } from "./utils"
+
+export default function BudgetCard ({name, amount, max}) {
+  return (
+    <Card>
+        <Card.Body>
+            <Card.Title>
+        <div>{name}</div>
+        <div>
+            {currencyFormatter.format()} / {currencyFormatter.format()}
+         </div>
+            </Card.Title>
+        </Card.Body>
+    </Card>
+  )
+}
+ 
